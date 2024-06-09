74d3c77ccb72338e325fb7f4daced6645b4cc5e9
MidoBadran <mohamedbadran1492008@gmail.com>
2024-06-09 17:02:02 +0300

    js form validation fixed

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,50 +6,74 @@ document.getElementById('form').addEventListener('submit', function (event) {
     // Get form fields
     const firstName = document.getElementById('firstName');
     const lastName = document.getElementById('lastName');
+    const email = document.getElementById('email');
     const queryType = document.querySelector('input[name="query"]:checked');
     const message = document.getElementById('message');
     const consent = document.getElementById('consent');
 
+
+    // Functions
+    function validateEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
     // Validate first name
     if (!firstName.value) {
         firstName.classList.add('invalid');
+        document.getElementById('firstName-p').style.display = 'block';
         formIsValid = false;
     } else {
         firstName.classList.remove('invalid');
+        document.getElementById('firstName-p').style.display = 'none';
     }
 
     // Validate last name
     if (!lastName.value) {
         lastName.classList.add('invalid');
+        document.getElementById('lastName-p').style.display = 'block';
         formIsValid = false;
     } else {
         lastName.classList.remove('invalid');
+        document.getElementById('lastName-p').style.display = 'none';
     }
+
+    // Validate Email
+    if (!validateEmail(email.value)) {
+        email.classList.add('invalid');
+        document.getElementById('email-p').style.display = 'block';
+    } else {
+        email.classList.remove('invalid');
+        document.getElementById('email-p').style.display = 'none';
+    }
+
 
     // Validate query type
     if (!queryType) {
-        document.getElementById('generalEnquiry').classList.add('invalid');
-        document.getElementById('supportRequest').classList.add('invalid');
+        document.getElementById('query-p').style.display = 'block';
         formIsValid = false;
     } else {
-        document.getElementById('generalEnquiry').classList.remove('invalid');
-        document.getElementById('supportRequest').classList.remove('invalid');
+        document.getElementById('query-p').style.display = 'none';
     }
 
     // Validate message
     if (!message.value) {
         message.classList.add('invalid');
+        document.getElementById('message-p').style.display = 'block';
         formIsValid = false;
     } else {
         message.classList.remove('invalid');
+        document.getElementById('message-p').style.display = 'none';
     }
 
     // Validate consent
     if (!consent.checked) {
         consent.classList.add('invalid');
+        document.getElementById('consent-p').style.display = 'block';
         formIsValid = false;
     } else {
         consent.classList.remove('invalid');
+        document.getElementById('consent-p').style.display = 'none';
     }
 
     if (formIsValid) {
